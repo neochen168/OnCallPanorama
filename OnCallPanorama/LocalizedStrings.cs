20db53cbab958510c27063fc790078d7dc6b3d96
ahan1874 <ahan1874@outlook.com>
2013-07-12 18:22:28 -0700

    Frist checkin

---
+++ OnCallPanorama/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using OnCallPanorama.Resources;
+
+namespace OnCallPanorama
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
